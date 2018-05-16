8f160e642970a05fa0c26031857acf72103836e7
SuperQifanWinAllTheTime <mr.y1994@hotmail.com>
2018-05-16 18:40:53 -0400

    finish
    finished the game

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour {
@@ -7,19 +8,37 @@ public class PlayerControl : MonoBehaviour {
     // Use this for initialization
     public float speed;
     private Rigidbody rb;
+    public Text countText;
+    public Text wintext;
+
+    private int score;
 
     // Update is called once per frame
     void Start()
     {
-        rb = GetComponent<Rigidbody>();    
+        rb = GetComponent<Rigidbody>();
+        score = 0;
+        setCountText();
+        wintext.text = "";
     }
-    void Update () {
-		
-	}
     void FixedUpdate () {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveHorizontal,0,moveVertical);
         rb.AddForce(movement*speed);
 	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up")) {
+            score = score + 1;
+            other.gameObject.SetActive(false);
+            setCountText();
+        }
+    }
+    void setCountText() {
+        countText.text = "Score: " + score.ToString();
+        if (score >= 13) {
+            wintext.text = "You Win";
+        }
+    }
 }
